fe7598389b645d54a01b26eb91a6a5eaebdaba5f
SachaEmmanuelTorres <redcaribou34@gmail.com>
2026-04-26 15:06:12 -0400

    refactorisation podman containerfile

--- notebook.py
+++ notebook.py
@@ -11,73 +11,12 @@
 
 import marimo
 
-__generated_with = "0.23.2"
+__generated_with = "0.23.3"
 app = marimo.App(width="medium")
 
 
 @app.cell
-def display_status():
-    return
-
-
-app._unparsable_cell(
-    r"""
-    # avec différentes couleurs et tailles de la lettre H (modifiez le nombre dans les parenthèses pour obtenir une autre taille)
-
-
-        def hello_world(nombre):  
-       for i in range(nombre):
-           print("Hello World")
-        def main():
-       hello_world(2)
-        if __name__=="__main__":main()
-
-        # How to use python code inside a jupyter notebook?
-
-        # To run Python code inside a Jupyter Notebook, you need to follow these steps:
-
-        #    1. Install Jupyter Notebook: You can install it by running the command `pip install jupyter` in your terminal or command prompt.
-        #    2. Launch Jupyter Notebook: Once installed, you can launch Jupyter Notebook by typing `jupyter notebook` in your terminal or command prompt. This will open a new window with the Jupyter Notebook interface.
-        #    3. Create a new notebook: In the Jupyter Notebook interface, click on "New" in the top right corner and select "Python 3 (or the version you have installed)" to create a new notebook.
-
-        # Now you can write and run Python code inside the notebook by creating cells and executing them. To do this, simply type your code in a cell and click the "Run" button or press Shift+Enter. The output will be displayed below the cell.
-    """,
-    name="_"
-)
-
-
-@app.cell
 def _():
-    from bs4 import BeautifulSoup
-    import requests
-
-    response = requests.get("https://www.lemonde.fr/pages/")
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    with open('lemonde.md', 'w') as f:
-        for link in soup.find_all('a'):
-            f.write(f"- [{link['href']}]({link['href']}) - {link.text}\n")
-    return
-
-
-@app.cell
-def _():
-    import requests
-    from bs4 import BeautifulSoup
-
-    base_url = "https://www.lemonde.fr/pages/?page="
-    response = requests.get(f"{base_url}1")
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    with open('lemonde.md', 'w') as f:
-        for link in soup.find_all('a'):
-            f.write(f"- [{link['href']}]({link['href']}) - {link.text}\n")
-        page = int(soup.select_one('.pagination a.next').text)
-        if page > 1:
-            response = requests.get(f"{base_url}{page}")
-            soup = BeautifulSoup(response.text, 'html.parser')
-            for link in soup.find_all('a'):
-                f.write(f"- [{link['href']}]({link['href']}) - {link.text}\n")
     return
 
 
